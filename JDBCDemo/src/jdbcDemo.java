1c7981317f272728c5dcfd1eeaba95e9c36f2f25
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2025-08-22 00:26:58 +0530

    last commit

--- JDBCDemo/src/jdbcDemo.java
+++ JDBCDemo/src/jdbcDemo.java
@@ -7,17 +7,26 @@ public class jdbcDemo {
         String url = "jdbc:postgresql://localhost:5432/Demo";
         String uname = "postgres";
         String pass = "aps2001";
+        String sql = "Select sname from student where sid = 1";
 
         //step 2 load and register driver      (Optional Step)
         Class.forName("org.postgresql.Driver");
 
         //step 3 create connection
         Connection con = DriverManager.getConnection(url, uname, pass);
+        System.out.println("connection ho gya");
+
 
         //step 4 create statement
         Statement st = con.createStatement();
 
+        // step 5 execute statement
+        ResultSet rs = st.executeQuery(sql);
+        System.out.println(rs.next());
 
-        System.out.println("connection ho gya");
+        //step 6 close connection
+        con.close();
+
+
     }
 }
