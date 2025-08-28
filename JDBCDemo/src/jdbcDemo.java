8c95bef4f71e6dba6adcdb94f9d5fc3ffc6059d5
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2025-08-29 00:35:09 +0530

    last commit

--- JDBCDemo/src/jdbcDemo.java
+++ JDBCDemo/src/jdbcDemo.java
@@ -3,12 +3,18 @@ import java.sql.*;          //step 1 - import package
 public class jdbcDemo {
     public static void main(String[] args) throws Exception {
 
+        //example for values used in preparedStatement
+        int sid = 4;
+        String sname = "Panda";
+        int marks = 95;
+
 
         String url = "jdbc:postgresql://localhost:5432/Demo";
         String uname = "postgres";
         String pass = "aps2001";
 //        String sql = "Select sname from student where sid = 1";
-        String sql = "select * from student";
+//        String sql = "select * from student";
+        String sql = "insert into student values (?,?,?)";      //sql query for prepared statement
 
         //step 2 load and register driver      (Optional Step)
         Class.forName("org.postgresql.Driver");
@@ -17,12 +23,19 @@ public class jdbcDemo {
         Connection con = DriverManager.getConnection(url, uname, pass);
         System.out.println("connection ho gya");
 
+        //Prepared Statement
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1,sid);
+        st.setString(2,sname);
+        st.setInt(3,marks);
+        st.execute();
+
 
         //step 4 create statement
-        Statement st = con.createStatement();
+//        Statement st = con.createStatement();
 
         // step 5 execute statement
-        ResultSet rs = st.executeQuery(sql);
+//        ResultSet rs = st.executeQuery(sql);
 
 //        rs.next();                               move pointer to first row
 //        String name = rs.getString("sname");          and will print the name
@@ -31,11 +44,11 @@ public class jdbcDemo {
 //        System.out.println(rs.next());    ture or false
 
         //to print the all data
-        while(rs.next()){
-            System.out.print(rs.getInt(1) + " ");
-            System.out.print(rs.getString(2) + " ");
-            System.out.println(rs.getInt(3) + " ");
-        }
+//        while(rs.next()){
+//            System.out.print(rs.getInt(1) + " ");
+//            System.out.print(rs.getString(2) + " ");
+//            System.out.println(rs.getInt(3) + " ");
+//        }
 
         //step 6 close connection
         con.close();
